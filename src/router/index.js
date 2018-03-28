3a6f729c0fbd0a39a35cabf384a55a2c473f0bc3
PeterChen <747853229@qq.com>
2018-03-28 22:44:51 +0800

    Fix router problem

--- src/router/index.js
+++ src/router/index.js
@@ -5,17 +5,41 @@ import Home from '@/components/frame/Home'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: Home
-    }
+const router = new Router({
+  routes: [{
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  }
   ]
 })
+
+router.beforeEach(async (to, from, next) => {
+  let userInfo = await localStorage.getItem('authUser') || '{"status": false}'
+  userInfo = JSON.parse(userInfo)
+  if (to.fullPath !== '/') {
+    if (userInfo.status) {
+      next()
+    } else {
+      next({
+        path: '/'
+      })
+    }
+  } else {
+    console.log(userInfo.status)
+    if (userInfo.status) {
+      next({
+        path: '/home'
+      })
+    } else {
+      next()
+    }
+  }
+})
+
+export default router
